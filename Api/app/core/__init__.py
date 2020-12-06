73cdf34f86c1a2223d09bb02070ce44faf03488d
Panudet Panumas <panudet.panumas@gmail.com>
2020-12-06 15:46:58 +0700

    fix cors

--- Api/app/core/__init__.py
+++ Api/app/core/__init__.py
@@ -15,13 +15,14 @@ def create_app():
     app.blueprint(api)
     bcrypt = Bcrypt(app)
     register_tortoise(app, generate_schemas=False ,config = postgres.config )
-    setup_cors(app)
+    CORS(app, automatic_options=True)
+    # setup_cors(app)
     return app
 
 
-def setup_cors(app):
-    @app.middleware('response')
-    async def allow_cross_site(request, response):
-        response.headers["Access-Control-Allow-Origin"] = "http://localhost:8080"
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Headers"] = "*"
+# def setup_cors(app):
+#     @app.middleware('response')
+#     async def allow_cross_site(request, response):
+#         response.headers["Access-Control-Allow-Origin"] = "http://localhost:8080"
+#         response.headers["Access-Control-Allow-Credentials"] = "true"
+#         response.headers["Access-Control-Allow-Headers"] = "*"
